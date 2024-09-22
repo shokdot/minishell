3f2f18ab034475e5e04fdb912cf3cd2f75fcbc8b
Hen Aleksanian (Shok.) <henula09106@gmail.com>
2024-09-23 00:54:48 +0400

    feat(readline): add readline, add_history

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,28 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/22 16:25:54 by healeksa          #+#    #+#             */
-/*   Updated: 2024/09/23 00:47:34 by healeksa         ###   ########.fr       */
+/*   Created: 2024/09/22 16:27:04 by healeksa          #+#    #+#             */
+/*   Updated: 2024/09/23 00:48:36 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
-int	main(int argc, char **argv, char **envp)
-{
-	char	*line;
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-	while (1)
-	{
-		line = readline(SH_PROMPT);
-		if (line == NULL)
-			break ;
-		if (line[0] != '\0')
-			add_history(line);
-		printf("%s\n", line);
-	}
-}
+# define SH_PROMPT "minishell>$ "
+
+#endif
