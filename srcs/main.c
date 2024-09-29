2c1da8cde5ea1810c989d61b793f1e20a78fad97
Hen Aleksanian (Shok.) <henula09106@gmail.com>
2024-09-29 14:20:56 +0400

    refactor: argument_check

--- srcs/main.c
+++ srcs/main.c
@@ -6,24 +6,29 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 16:25:54 by healeksa          #+#    #+#             */
-/*   Updated: 2024/09/29 14:14:28 by healeksa         ###   ########.fr       */
+/*   Updated: 2024/09/29 14:19:44 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	argument_check(int argc, char **argv)
+{
+	if (argc != 1 || !argv[0])
+	{
+		ft_putendl_fd("Minishell can't take argumensts.", 2);
+		exit(EXIT_FAILURE);
+	}
+}
 
 int	main(int argc, char **argv, char **envp)
 {
 	char	*line;
 	t_shell	minshell;
 
-	if (argc != 1 || !argv[0])
-	{
-		ft_putendl_fd("Minishell can't take argumensts.", 2);
-		exit(EXIT_FAILURE);
-	}
-	shell_init(&minshell);
 	(void)envp;
+	argument_check(argc, argv);
+	shell_init(&minshell);
 	while (1)
 	{
 		line = readline(PROMPT);
